18e541d6a1e0f6d6ff60c24d5647026d7efede4a
Keks <keks@htmlacademy.ru>
2022-03-24 09:09:49 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/util.js
@@ -0,0 +1,31 @@
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+function getRandomPositiveFloat (a, b, digits = 1) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return +result.toFixed(digits);
+}
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+function mixElements(elements) {
+  elements.forEach((element, index, items) => {
+    const randomIndex = getRandomPositiveInteger(0, elements.length - 1);
+    const current = items[index];
+    items[index] = items[randomIndex];
+    items[randomIndex] = current;
+  });
+  return elements;
+}
+
+function getRandomStringFromArray(elements) {
+  return mixElements(elements).slice(0, getRandomPositiveInteger(1, elements.length - 1)).join(', ');
+}
+
+export {getRandomPositiveInteger, getRandomPositiveFloat, getRandomArrayElement, getRandomStringFromArray};
