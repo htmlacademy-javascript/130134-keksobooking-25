e8a00d7d403105de96f0ba72c59523085b0d4a9d
Ivan Klimukhin <ivankmn@yandex.ru>
2022-03-31 22:12:15 +0300

    Добавил карту и активацию страницы после инициализации карты

--- js/main.js
+++ js/main.js
@@ -5,13 +5,43 @@ import {deactivateForms, activateForms} from './form.js';
 const mapContainer = document.querySelector('#map-canvas');
 const adList = getAdList();
 const cardList = generateCards(adList);
-mapContainer.append(cardList.querySelector('.popup'));
 
 
-// deactivateForms();
+deactivateForms();
 
 // document.addEventListener('keypress', (evt) => {
 //   if(evt.key === 'Enter') {
 //     activateForms();
 //   }
 // }, { once: true });
+
+
+const map = L.map('map-canvas')
+  .on('load', () => {
+    activateForms();
+  }).setView({
+    lat: 35.65960192064806,
+    lng: 139.74541672508437,
+  }, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const marker = L.marker(
+  {
+    lat: 35.65960192064806,
+    lng: 139.74541672508437,
+  },
+  {
+    draggable: true,
+  }
+);
+
+
+marker.addTo(map);
+
+
